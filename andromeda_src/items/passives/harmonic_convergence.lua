dfc2f6984a32603b4237263f6a93431b0b026fe1
Hybrid <infinitelyuda@gmail.com>
2022-11-07 13:28:01 -0500

    Fixed getting parent for lasers/bombs

--- andromeda_src/items/passives/harmonic_convergence.lua
+++ andromeda_src/items/passives/harmonic_convergence.lua
@@ -26,61 +26,61 @@ function Item.postFireTear(tear)
 end
 
 function Item.postLaserInit(laser)
+	if laser.SpawnerEntity == nil then return end
 	if laser.SpawnerType ~= EntityType.ENTITY_PLAYER then return end
-	
-	for i = 0, game:GetNumPlayers() - 1 do
-		local player = Isaac.GetPlayer(i)
-		
-		if not player:HasCollectible(Enums.Collectibles.HARMONIC_CONVERGENCE) then return end
 
-		if laser.Variant ~= 3
-		and laser.Variant ~= 7
-		and laser.Variant ~= 8
-		and laser.Variant ~= 10
-		then
-			local angle = 0
-		
-			if convergence % 2 ~= 0 then
-				angle = 45
-			end
-			
-			for i = 0, 3 do
-				local dmgDivider = 1
-				angle = angle + (90 * i)
-				
-				if laser.Variant == 2 then
-					dmgDivider = 4
-				end
-				
-				Functions.ConvergingTears(laser, player, player.Position, angle, dmgDivider, false)
-			end
-			convergence = convergence + 1
-		end
-	end
-end
+	local player = laser.SpawnerEntity:ToPlayer()
 
-function Item.postBombUpdate(bomb)
-	if bomb.SpawnerType ~= EntityType.ENTITY_PLAYER then return end
-	if not bomb.IsFetus then return end
-	if bomb.FrameCount ~= 1 then return end
+	if player == nil then return end
+	if not player:HasCollectible(Enums.Collectibles.HARMONIC_CONVERGENCE) then return end
 	
-	for i = 0, game:GetNumPlayers() - 1 do
-		local player = Isaac.GetPlayer(i)
-		
-		if not player:HasCollectible(Enums.Collectibles.HARMONIC_CONVERGENCE) then return end
-		
+	if laser.Variant ~= 3
+	and laser.Variant ~= 7
+	and laser.Variant ~= 8
+	and laser.Variant ~= 10
+	then
 		local angle = 0
-		
+	
 		if convergence % 2 ~= 0 then
 			angle = 45
 		end
 		
 		for i = 0, 3 do
+			local dmgDivider = 1
 			angle = angle + (90 * i)
-			Functions.ConvergingTears(bomb, player, player.Position, angle, 4, false)
+			
+			if laser.Variant == 2 then
+				dmgDivider = 4
+			end
+			
+			Functions.ConvergingTears(laser, player, player.Position, angle, dmgDivider, false)
 		end
 		convergence = convergence + 1
 	end
+end
+
+function Item.postBombUpdate(bomb)
+	if bomb.FrameCount ~= 1 then return end
+	if not bomb.IsFetus then return end
+	if bomb.Parent == nil then return end
+	if bomb.Parent.Type ~= EntityType.ENTITY_PLAYER then return end
+
+	local player = bomb.Parent:ToPlayer()
+
+	if player == nil then return end
+	if not player:HasCollectible(Enums.Collectibles.HARMONIC_CONVERGENCE) then return end
+
+	local angle = 0
+	
+	if convergence % 2 ~= 0 then
+		angle = 45
+	end
+	
+	for i = 0, 3 do
+		angle = angle + (90 * i)
+		Functions.ConvergingTears(bomb, player, player.Position, angle, 4, false)
+	end
+	convergence = convergence + 1
 end
 
 function Item.preTearCollision(tear, collider, low)
