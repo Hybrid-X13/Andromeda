4f9b26f49f476f7fb4f7cc54d9cea8764dca1476
Hybrid <infinitelyuda@gmail.com>
2022-12-04 15:58:20 -0500

    Added callbacks

--- andromeda_src/callbacks/post_effect_update.lua
+++ andromeda_src/callbacks/post_effect_update.lua
@@ -2,8 +2,10 @@ local Andromeda = require("andromeda_src.characters.andromeda")
 local T_Andromeda = require("andromeda_src.characters.t_andromeda")
 local Ceres = require("andromeda_src.items.passives.ceres")
 local LuminaryFlare = require("andromeda_src.items.passives.luminary_flare")
+local HarmonicConvergence = require("andromeda_src.items.passives.harmonic_convergence")
 local AlienTransmitter = require("andromeda_src.items.trinkets.alien_transmitter")
 local AbandonedPlanetarium = require("andromeda_src.misc.abandoned_planetarium")
+local Spode = require("andromeda_src.misc.spode")
 
 local function MC_POST_EFFECT_UPDATE(_, effect)
 	Andromeda.postEffectUpdate(effect)
@@ -11,10 +13,12 @@ local function MC_POST_EFFECT_UPDATE(_, effect)
 	
 	Ceres.postEffectUpdate(effect)
 	LuminaryFlare.postEffectUpdate(effect)
+	HarmonicConvergence.postEffectUpdate(effect)
 
 	AlienTransmitter.postEffectUpdate(effect)
 
 	AbandonedPlanetarium.postEffectUpdate(effect)
+	Spode.postEffectUpdate(effect)
 end
 
 return MC_POST_EFFECT_UPDATE
