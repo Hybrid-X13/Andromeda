993e28f498c9ec28b9bb0012ec78e6413ec61393
Hybrid <infinitelyuda@gmail.com>
2022-12-27 00:53:53 -0500

    Added Alien Transmitter

--- andromeda_src/callbacks/post_effect_init.lua
+++ andromeda_src/callbacks/post_effect_init.lua
@@ -1,9 +1,12 @@
 local LuminaryFlare = require("andromeda_src.items.passives.luminary_flare")
 local Vesta = require("andromeda_src.items.passives.vesta")
+local AlienTransmitter = require("andromeda_src.items.trinkets.alien_transmitter")
 
 local function MC_POST_EFFECT_INIT(_, effect)
 	LuminaryFlare.postEffectInit(effect)
 	Vesta.postEffectInit(effect)
+
+	AlienTransmitter.postEffectInit(effect)
 end
 
 return MC_POST_EFFECT_INIT
