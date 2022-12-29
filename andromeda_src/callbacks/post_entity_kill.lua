c1c86f3540da44e50aae046ed085eef748dc0aa4
Hybrid <infinitelyuda@gmail.com>
2022-12-28 23:15:00 -0500

    Removed Ophiuchus

--- andromeda_src/callbacks/post_entity_kill.lua
+++ andromeda_src/callbacks/post_entity_kill.lua
@@ -1,11 +1,8 @@
 local UnlockManager = require("andromeda_src.unlock_manager")
-local Ophiuchus = require("andromeda_src.items.passives.ophiuchus")
 local Wisp = require("andromeda_src.misc.custom_wisps")
 
 local function MC_POST_ENTITY_KILL(_, entity)
 	UnlockManager.postEntityKill(entity)
-	
-	Ophiuchus.postEntityKill(entity)
 
 	Wisp.postEntityKill(entity)
 end
