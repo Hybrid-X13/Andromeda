3762525b7fd1c7f5c64908c4d9ae35184ce4e8c0
Hybrid <infinitelyuda@gmail.com>
2022-12-28 23:15:11 -0500

    Added Ophiuchus

--- andromeda_src/callbacks/post_npc_death.lua
+++ andromeda_src/callbacks/post_npc_death.lua
@@ -1,5 +1,6 @@
 local UnlockManager = require("andromeda_src.unlock_manager")
 local Juno = require("andromeda_src.items.passives.juno")
+local Ophiuchus = require("andromeda_src.items.passives.ophiuchus")
 local LuminaryFlare = require("andromeda_src.items.passives.luminary_flare")
 local Stardust = require("andromeda_src.items.trinkets.stardust")
 
@@ -7,6 +8,7 @@ local function MC_POST_NPC_DEATH(_, npc)
 	UnlockManager.postNPCDeath(npc)
 	
 	Juno.postNPCDeath(npc)
+	Ophiuchus.postNPCDeath(npc)
 	LuminaryFlare.postNPCDeath(npc)
 	
 	Stardust.postNPCDeath(npc)
