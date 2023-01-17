b04b6b11770db1363102843291e15c4276fab0d9
Hybrid <infinitelyuda@gmail.com>
2023-01-17 13:28:15 -0500

    Added Spode

--- andromeda_src/callbacks/post_player_update.lua
+++ andromeda_src/callbacks/post_player_update.lua
@@ -2,6 +2,7 @@ local UnlockManager = require("andromeda_src.unlock_manager")
 local Andromeda = require("andromeda_src.characters.andromeda")
 local T_Andromeda = require("andromeda_src.characters.t_andromeda")
 local Juno = require("andromeda_src.items.passives.juno")
+local Spode = require("andromeda_src.misc.spode")
 
 local function MC_POST_PLAYER_UPDATE(_, player)
 	UnlockManager.postPlayerUpdate(player)
@@ -10,6 +11,8 @@ local function MC_POST_PLAYER_UPDATE(_, player)
 	T_Andromeda.postPlayerUpdate(player)
 	
 	Juno.postPlayerUpdate(player)
+
+	Spode.postPlayerUpdate(player)
 end
 
 return MC_POST_PLAYER_UPDATE
