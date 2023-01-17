660b64a07d6104bae6f2de122332c70de740008e
Hybrid <infinitelyuda@gmail.com>
2023-01-17 13:48:10 -0500

    Added Spode

--- andromeda_src/callbacks/post_new_room.lua
+++ andromeda_src/callbacks/post_new_room.lua
@@ -7,6 +7,7 @@ local LuminaryFlare = require("andromeda_src.items.passives.luminary_flare")
 local Sextant = require("andromeda_src.items.trinkets.sextant")
 local MoonStone = require("andromeda_src.items.trinkets.moon_stone")
 local AbandonedPlanetarium = require("andromeda_src.misc.abandoned_planetarium")
+local Spode = require("andromeda_src.misc.spode")
 local WispWizard = require("andromeda_src.misc.wisp_wizard")
 local Wisp = require("andromeda_src.misc.custom_wisps")
 
@@ -24,6 +25,7 @@ local function MC_POST_NEW_ROOM()
 	MoonStone.postNewRoom()
 	
 	AbandonedPlanetarium.postNewRoom()
+	Spode.postNewRoom()
 	WispWizard.postNewRoom()
 	Wisp.postNewRoom()
 end
