5fdf077cdf030d6add523674039fec0bf3eb3fde
Hybrid <infinitelyuda@gmail.com>
2023-02-01 14:01:40 -0500

    Changed priority of some callbacks

--- andromeda_src/callbacks/entity_take_dmg.lua
+++ andromeda_src/callbacks/entity_take_dmg.lua
@@ -1,6 +1,4 @@
 local BookOfCosmos = require("andromeda_src.items.actives.book_of_cosmos")
-local Ophiuchus = require("andromeda_src.items.passives.ophiuchus")
-local Juno = require("andromeda_src.items.passives.juno")
 local Ceres = require("andromeda_src.items.passives.ceres")
 local Vesta = require("andromeda_src.items.passives.vesta")
 local Chiron = require("andromeda_src.items.passives.chiron")
@@ -17,12 +15,6 @@ local function MC_ENTITY_TAKE_DMG(_, entity, amount, flags, source, countdown)
 	if returned ~= nil then return returned end
 	
 	local returned = BookOfCosmos.entityTakeDmg(entity, amount, flags, source, countdown)
-	if returned ~= nil then return returned end
-
-	local returned = Ophiuchus.entityTakeDmg(entity, amount, flags, source, countdown)
-	if returned ~= nil then return returned end
-
-	local returned = Juno.entityTakeDmg(entity, amount, flags, source, countdown)
 	if returned ~= nil then return returned end
 
 	local returned = Wisp.entityTakeDmg(entity, amount, flags, source, countdown)
--- andromeda_src/callbacks/priority/entity_take_dmg.lua
+++ andromeda_src/callbacks/priority/entity_take_dmg.lua
@@ -1,11 +1,19 @@
 local Andromeda = require("andromeda_src.characters.andromeda")
 local T_Andromeda = require("andromeda_src.characters.t_andromeda")
+local Ophiuchus = require("andromeda_src.items.passives.ophiuchus")
+local Juno = require("andromeda_src.items.passives.juno")
 
 local function MC_ENTITY_TAKE_DMG(_, entity, amount, flags, source, countdown)
 	local returned = Andromeda.entityTakeDmg(entity, amount, flags, source, countdown)
 	if returned ~= nil then return returned end
 	
 	local returned = T_Andromeda.entityTakeDmg(entity, amount, flags, source, countdown)
+	if returned ~= nil then return returned end
+
+	local returned = Ophiuchus.entityTakeDmg(entity, amount, flags, source, countdown)
+	if returned ~= nil then return returned end
+
+	local returned = Juno.entityTakeDmg(entity, amount, flags, source, countdown)
 	if returned ~= nil then return returned end
 end
 
