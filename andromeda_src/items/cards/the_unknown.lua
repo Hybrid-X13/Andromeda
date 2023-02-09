0144c8a5aa763f761c0cfd03eb11f625455f0146
Hybrid <infinitelyuda@gmail.com>
2023-02-09 12:49:49 -0500

    No longer able to teleport to the error room

--- andromeda_src/items/cards/the_unknown.lua
+++ andromeda_src/items/cards/the_unknown.lua
@@ -1,5 +1,4 @@
 local Enums = require("andromeda_src.enums")
-local game = Game()
 local rng = RNG()
 
 local Consumable = {}
@@ -8,20 +7,14 @@ function Consumable.useCard(card, player, flag)
 	if card ~= Enums.Cards.THE_UNKNOWN then return end
 	
 	local rng = player:GetCardRNG(Enums.Cards.THE_UNKNOWN)
-	local randNum = rng:RandomInt(20)
+	local cardNum = rng:RandomInt(22) + 1
+	local randNum = rng:RandomInt(2)
 	
 	if randNum == 0 then
-		game:StartRoomTransition(GridRooms.ROOM_ERROR_IDX, Direction.NO_DIRECTION, RoomTransitionAnim.TELEPORT, player, -1)
-	else
-		local cardNum = rng:RandomInt(22) + 1
-		randNum = rng:RandomInt(2)
-		
-		if randNum == 0 then
-			--Reverse tarot card
-			cardNum = cardNum + 55
-		end
-		player:UseCard(cardNum, UseFlag.USE_NOANIM)
+		cardNum = cardNum + 55
 	end
+
+	player:UseCard(cardNum, UseFlag.USE_NOANIM)
 end
 
 return Consumable
