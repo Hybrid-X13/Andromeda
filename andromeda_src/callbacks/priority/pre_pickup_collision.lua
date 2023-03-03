5bc8aa32d4ffca6ed9fd63bc1f2746070b333f53
Hybrid <infinitelyuda@gmail.com>
2023-03-02 23:43:46 -0500

    Reverted pre pickup collision priority

--- andromeda_src/callbacks/priority/pre_pickup_collision.lua
+++
@@ -1,8 +0,0 @@
-local Singularity = require("andromeda_src.items.actives.singularity")
-
-local function MC_PRE_PICKUP_COLLISION(_, entity, collider, low)
-	local returned = Singularity.prePickupCollision(entity, collider, low)
-	if returned ~= nil then return returned end
-end
-
-return MC_PRE_PICKUP_COLLISION
