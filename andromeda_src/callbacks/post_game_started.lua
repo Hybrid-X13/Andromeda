5e7ae49298a7584c5522b4ac198aad63a3ea7065
Hybrid <infinitelyuda@gmail.com>
2023-04-14 14:58:48 -0400

    Removed callback

--- andromeda_src/callbacks/post_game_started.lua
+++ andromeda_src/callbacks/post_game_started.lua
@@ -1,11 +1,9 @@
 local FFCompat = require("andromeda_src.compat.fiend_folio")
 local HCCompat = require("andromeda_src.compat.heavens_call")
-local BirthcakeCompat = require("andromeda_src.compat.birthcake")
 
 local function MC_POST_GAME_STARTED(_, isContinue)
 	FFCompat.postGameStarted(isContinue)
 	HCCompat.postGameStarted(isContinue)
-	BirthcakeCompat.postGameStarted(isContinue)
 end
 
 return MC_POST_GAME_STARTED
