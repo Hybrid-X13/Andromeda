828e40248950a95605d5b1bbe0c1635e33d0dd5e
Hybrid <infinitelyuda@gmail.com>
2023-04-14 02:08:59 -0400

    Added callbacks

--- andromeda_src/callbacks/post_npc_death.lua
+++ andromeda_src/callbacks/post_npc_death.lua
@@ -1,4 +1,5 @@
 local UnlockManager = require("andromeda_src.unlock_manager")
+local Andromeda = require("andromeda_src.characters.andromeda")
 local Juno = require("andromeda_src.items.passives.juno")
 local Ophiuchus = require("andromeda_src.items.passives.ophiuchus")
 local LuminaryFlare = require("andromeda_src.items.passives.luminary_flare")
@@ -6,6 +7,8 @@ local Stardust = require("andromeda_src.items.trinkets.stardust")
 
 local function MC_POST_NPC_DEATH(_, npc)
 	UnlockManager.postNPCDeath(npc)
+	
+	Andromeda.postNPCDeath(npc)
 	
 	Juno.postNPCDeath(npc)
 	Ophiuchus.postNPCDeath(npc)
