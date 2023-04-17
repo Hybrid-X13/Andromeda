5b5f15537fd286f24a2c94cabbcbb2cb067f6229
Hybrid <infinitelyuda@gmail.com>
2023-04-17 01:33:48 -0400

    Fixed special layoouts not appearing

--- andromeda_src/items/cards/soul_of_andromeda.lua
+++ andromeda_src/items/cards/soul_of_andromeda.lua
@@ -9,7 +9,7 @@ function Consumable.useCard(card, player, flag)
 	
 	local rng = player:GetCardRNG(Enums.Cards.SOUL_OF_ANDROMEDA)
 
-	ANDROMEDA:GoToAbandonedPlanetarium(player, 0.333, false)
+	ANDROMEDA:GoToAbandonedPlanetarium(player, 0.333, true)
 	Functions.PlayVoiceline(Enums.Voicelines.SOUL_OF_ANDROMEDA, flag, rng:RandomInt(2))
 end
 
