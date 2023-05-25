521eedffa6bebed80c6a697a7512649093eacb1a
Hybrid <infinitelyuda@gmail.com>
2023-05-25 12:48:51 -0400

    Scrapped secret room idea

--- andromeda_src/callbacks/post_update.lua
+++ andromeda_src/callbacks/post_update.lua
@@ -1,12 +1,10 @@
 local AlienTransmitter = require("andromeda_src.items.trinkets.alien_transmitter")
 local AbandonedPlanetarium = require("andromeda_src.misc.abandoned_planetarium")
-local Secret = require("andromeda_src.misc.secret")
 
 local function MC_POST_UPDATE()
 	AlienTransmitter.postUpdate()
 	
 	AbandonedPlanetarium.postUpdate()
-	Secret.postUpdate()
 end
 
 return MC_POST_UPDATE
