350a66b0a1620edac6fcbadcc57872ae26d305f2
Hybrid <infinitelyuda@gmail.com>
2023-06-29 02:01:05 -0400

    Blacklisted certain modded card from adding charge

--- andromeda_src/items/actives/singularity.lua
+++ andromeda_src/items/actives/singularity.lua
@@ -499,6 +499,7 @@ function Item.useCard(card, player, flag)
 	if card == Isaac.GetCardIdByName("Storage Battery (2)") then return end
 	if card == Isaac.GetCardIdByName("Storage Battery (Full)") then return end
 	if card == Isaac.GetCardIdByName("Corroded Battery") then return end
+	if card == Isaac.GetCardIdByName("Pot of Greed") then return end
 	if card == Isaac.GetCardIdByName("Denial's D9") then return end
 
 	if card == Isaac.GetCardIdByName("Quasar Shard") then
